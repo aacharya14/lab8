be975be476e02cb5b73e0a919e5bac882a81b79a
Ambika <aacharya@stanford.edu>
2014-02-26 23:09:03 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,9 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(sendAnalysis);
+}
+
+function sendAnalysis(e){
+	ga("send", "event", "like", "click");
 }
